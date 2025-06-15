0debb3e6a9a4e3a8c50f6bb4b49447314341528e
agastya1033 <agastya1033@gmail.com>
2025-06-15 19:01:28 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,31 +1,36 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserTest {
 
-	
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
-		driver.close();
-	
-		
-		
-	}
+    @Test
+    public void getData() {
+        System.out.println("Running Selenium Test...");
+
+        // ✅ Automatically sets up chromedriver version
+        WebDriverManager.chromedriver().setup();
+
+        // ✅ Enable headless mode for Azure DevOps agents
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // or "--headless"
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://rahulshettyacademy.azurewebsites.net/webapp/");
+        String text = driver.findElement(By.cssSelector("h1")).getText();
+        System.out.println("Header Text: " + text);
+
+        Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+
+        driver.quit();
+    }
 }
